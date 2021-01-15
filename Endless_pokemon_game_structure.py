c812eaad96b08966f24785ed828178ee01402e86
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2021-01-15 16:50:17 +0530

    Update Endless_pokemon_game_structure.py

--- Endless_pokemon_game_structure.py
+++ Endless_pokemon_game_structure.py
@@ -6,9 +6,18 @@ root=Tk()
 root.title("Endless Dice Game")
 root.geometry("700x500")
 root.configure(background="orange")
+Pickachu=ImageTk.PhotoImage(Image.open ("Pikachu.jpg"))
+Bulbasor=ImageTk.PhotoImage(Image.open ("bulbasour.jpg"))
+Abra=ImageTk.PhotoImage(Image.open ("Abra.jpg"))
+Charmender=ImageTk.PhotoImage(Image.open ("Charmender.jpg"))
+
+Iyvasour=ImageTk.PhotoImage(Image.open ("Iyvasour.jpg"))
+Jigglypuff=ImageTk.PhotoImage(Image.open ("Jigglypuff.jpg"))
+Kadabra=ImageTk.PhotoImage(Image.open ("Kadabra.jpg"))
+Nidoking=ImageTk.PhotoImage(Image.open ("Nidoking.jpg"))
 img=ImageTk.PhotoImage(Image.open ("button.jpg"))
-pokemon_list=["Pikachu", "Bulbasor","Abra","Bulbasaur","Charmender","Iyvasour","Jigglypuff","Kadabra","Nidoking"]
-Power_pokemon=[200, 60,30,60,50,100,70,70,90]
+pokemon_list=[Pickachu,Bulbasor,Abra,Charmender,Iyvasour,Jigglypuff,Kadabra,Nidoking]
+Power_pokemon=[200, 60,30,50,100,70,70,90]
 player1 = Label(root, text = "Player 1", bg = "red", fg = "white")
 player1.place(relx = 0.1, rely = 0.3 , anchor =  CENTER)
 
@@ -30,6 +39,7 @@ def player1():
     random_poke_label["text"] = ""
     player1_score = player1_score + Power_pokemon[random_no]
     player_1_score_label["text"] = str( player1_score)
+    random_poke_label["image"]=pokemon_list[random_no]
     
 player_1_btn = Button(root, image = img, command = player1)
 player_1_btn.place(relx = 0.1, rely = 0.6 , anchor =  CENTER)
@@ -41,6 +51,8 @@ def player2():
     random_poke_label["text"] = ""
     player2_score = player2_score + Power_pokemon[random_no2]
     player_2_score_label["text"] = str( player2_score)
+    random_poke_label["image"]=pokemon_list[random_no2]
+    
     
 
 player_2_btn = Button(root, image = img, command = player2)
